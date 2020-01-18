6549cf10cd603e397e37394ba1fc079b4f3f799a
colinaverill <colin.averill@gmail.com>
2020-01-18 11:22:14 -0600

    added new covariates to spatial variable extraction. More worldclim2 bioclim variables, and decomp variables calculated from them. Based on Steidinger et al. 2019, Nature.

--- 1._data_construction/3._assign_spatial_product_data.r
+++ 1._data_construction/3._assign_spatial_product_data.r
@@ -14,7 +14,9 @@ p1 <- data.table(readRDS(Product_1.path))
 p2 <- data.table(readRDS(Product_2.path))
 
 #remove spatial columns if already present.----
-to.drop <- c('n.dep','wet.dep','dry.dep','mat','map','mat_CV','map_CV','mat_sd','map_sd','mdr')
+to.drop <- c("n.dep","ndep","wet.dep","dry.dep",
+             "mat","t_seas","t_range","t_wet_q","t_warm_q","map","p_wet_q","p_warm_q",
+             "decomp_wet_q","decomp_warm_q")
 p1 <- p1[,c(to.drop) := NULL]
 p2 <- p2[,c(to.drop) := NULL]
 
@@ -24,8 +26,21 @@ setnames(p1, 'n.dep','ndep')
 p2 <- merge(p2,p1[,c('PLT_CN','ndep','dry.dep','wet.dep')])
 
 #pull worldclim2 climate data.----
-p1 <- cbind(p1,worldclim2_grab(p1$latitude,p1$longitude))
-p2 <- merge(p2, p1[,c('PLT_CN','mat','map')])
+wc2 <- worldclim2_grab(p1$latitude, p1$longitude)
+p1 <- cbind(p1, wc2)
+col.names <- c('PLT_CN', colnames(wc2))
+p1 <- as.data.frame(p1)
+p2 <- merge(p2, p1[,col.names])
+
+#calculate decomposition in warmest and wettest quarters, following Steidinger et al. 2019, Nature.----
+#calculation from Yasso7 model.
+decomp_calc <- function(temp, prec){
+  k <- exp(0.095*temp - 0.00014 * temp^2) * (1 - exp(-1.21 * prec))
+  return(k)
+}
+p1$decomp_wet_q  <- decomp_calc(p1$t_wet_q , p1$p_wet_q )
+p1$decomp_warm_q <- decomp_calc(p1$t_warm_q, p1$p_warm_q)
+p2 <- merge(p2, p1[,c('PLT_CN','decomp_wet_q','decomp_warm_q')])
 
 #save output.----
 saveRDS(p1, Product_1.path, version = 2)
